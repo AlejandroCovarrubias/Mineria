1eecac4ff39bf5b4487a99e96d70a1460612fec7
Alejandro Galindo Covarrubias <alejandrogacova@gmai.com>
2020-03-26 00:36:11 -0700

    Pues alli va algo

---
+++ Semaforo/src/semaforo/SemaforoEP.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semaforo;
+
+import javax.websocket.ClientEndpoint;
+
+/**
+ *
+ * @author Alejandro Galindo
+ */
+@ClientEndpoint
+public class SemaforoEP {
+    
+}
